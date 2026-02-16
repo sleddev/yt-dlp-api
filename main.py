d8c48d9ca9f0c2c63789886bcf5d46c4df1a0e23
SledDev <sleddev@asd.dog>
2026-02-16 23:37:59 +0100

    mp4 take 2

--- main.py
+++ main.py
@@ -40,10 +40,7 @@ def get_video(url: str, password: str, setfilename: bool = True):
         "remote_components": ["ejs:github","ejs:npm"],
         "ffmpeg_location": "/usr/local/bin/ffmpeg",
         "verbose": True,
-        'postprocessors': [{
-            'key': 'FFmpegVideoConvertor',
-            'preferedformat': 'mp4',  # This will convert WebM to MP4 after download
-        }],
+        'merge_output_format': 'mp4'
     }
 
     if password != os.environ.get("DOWNLOAD_PASS"):
